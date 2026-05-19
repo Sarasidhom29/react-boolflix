521d6acea193b632f2c78f08a655df19f3228859
Sara Sidhom <s.sara.29.07.2006@gmail.com>
2026-05-19 21:34:37 +0200

    importato provider in main

--- src/main.jsx
+++ src/main.jsx
@@ -8,11 +8,17 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 import './styles/index.css'
 
+import { BoolflixProvider } from './contexts/BoolflixContext.jsx'
+
 
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+
+    <BoolflixProvider>
+      <App />
+    </BoolflixProvider>
+
   </StrictMode>,
 )
